320a88f3e308c87008b41cfe22b81c9b28490a4b
Mitch Powell <mitch.powell@willowtreeapps.com>
2017-08-03 14:37:50 -0400

    fix bad js

--- big-boi.js
+++ big-boi.js
@@ -2,28 +2,29 @@
 
     const savedState = state['saved-state'];
     const lastMove = savedState['last-move'];
-    const commands = {
-      'turnCommand': turnCommand,
-      'shootCommand': shootCommand,
-    }
 
     const turnCommand = {
       action: 'turn',
       metadata: {
         direction: 'right',
       },
-    }
+    };
 
     const shootCommand = {
       action: 'shoot',
       metadata: {},
-    }
+    };
 
     const nextCommand = (
       lastMove == 'turnCommand'
         ? 'shootCommand'
         : 'turnCommand'
-    )
+    );
+
+    const commands = {
+      'turnCommand': turnCommand,
+      'shootCommand': shootCommand,
+    };
 
     return {
         command: commands[nextCommand],

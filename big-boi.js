18d0ac98d94a67e55432bb6421629fe35d519810
Mitch Powell <mitch.powell@willowtreeapps.com>
2017-11-25 16:20:15 -0500

    fix the big boi

--- big-boi.js
+++ big-boi.js
@@ -1,12 +1,34 @@
 ((state, timeLeftFn) => {
-    let command;
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://wombackend.herokuapp.com', false);
-    xhr.send();
-//    http.get('http://wombackend.herokuapp.com/', res => {
-//        res.on('data', data => { 
-//            command = JSON.parse(data);
-//            return { command, state: {} }
-//        })
-//    });
+
+    const savedState = state['saved-state'];
+    const lastMove = savedState['last-move'];
+    const commands = {
+      'turnCommand': turnCommand,
+      'shootCommand': shootCommand,
+    }
+
+    const turnCommand = {
+      action: 'turn',
+      metadata: {
+        direction: 'right',
+      },
+    }
+
+    const shootCommand = {
+      action: 'shoot',
+      metadata: {},
+    }
+
+    const nextCommand = (
+      lastMove == 'turnCommand'
+        ? 'shootCommand'
+        : 'turnCommand'
+    )
+
+    return {
+        command: commands[nextCommand],
+        state: {
+          'last-move': nextCommand
+        }
+    };
 });
